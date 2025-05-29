97c7510465d3548333e5f3d9b2acbf93eab17fe8
zimon854 <rudfhr71@gmail.com>
2025-05-29 15:35:39 +0900

    Next.js 14 배포 호환: nextauth route export 방식 수정

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,5 @@ import { authConfig } from "@/lib/auth";
 
 const handler = NextAuth(authConfig);
 
-export { handler as GET, handler as POST }; 
+export const GET = handler;
+export const POST = handler; 
